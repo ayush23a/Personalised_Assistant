93f25661e205ce41cf2ccd134fd81d95789f0e84
Ayush Aman <140056729+ayush23a@users.noreply.github.com>
2024-05-19 17:27:04 +0530

    imported_modules.py

---
+++ imported_modules.py
@@ -0,0 +1,12 @@
+## for assistant design
+from pydoc import text
+import pyttsx3
+import speech_recognition as sr 
+import datetime
+import wikipedia
+import webbrowser
+from google_trans_new import google_translator
+
+# for GUI design and backend connection
+import os
+import eel   # frontend backend connector for python
